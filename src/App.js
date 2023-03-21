ad594df2e6a16658d03398383f4499266af2330c
Sameer Siddiqui <sameersiddiqui3540@gmail.com>
2023-03-21 19:20:11 +0530

    Implementation of useEffect Hook (v0.0)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 function App() {
-  const [count,setCount] = useState(0);
-  const decrementCount = () => {
-    setCount(count => count-1);
-    setCount(count => count-1);
-  }
-  const incrementCount = () => {
-    setCount(count => count+1);
-    setCount(count => count+1);
-    setCount(count => count+1);
-  }
+  const [resourceType, setResourceType] = useState('posts');
+
+  /*
+  This useEffect will run everytime our application renders (we click on any button).
+  useEffect takes an array as second parameter, and it will run 
+  whenever there is any change in any of the value of that array.
+
+  If we pass [] as second parameter, it will act as mount function 
+  and it will only run on the first render.
+  */ 
+
+  // This will run only for the first time.
+  
+  // useEffect(() => {
+  //   console.log("Started!")
+  // },[resourceType])
+
+  // This will run whenever there is any change in the value of resourceType
+  useEffect(() => {
+    console.log("Started!")
+  },[resourceType])
+
   return (
     <div className="App">
-      <button onClick={decrementCount}>-</button>
-      <span>{count}</span>
-      <button onClick={incrementCount}>+</button>
+      
+      <button onClick={() => setResourceType('posts')}>Posts</button>
+      <button onClick={() => setResourceType('users')}>Users</button>
+      <button onClick={() => setResourceType('comments')}>Comments</button>
+
+      <h1>{resourceType}</h1>
     </div>
+
   );
 }
 
