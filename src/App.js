025cc1b3ad3fd773c1e59515f249175eb874ba5d
Sameer Siddiqui <sameersiddiqui3540@gmail.com>
2023-03-21 18:43:45 +0530

    Implementation of useState Hook (v1.0)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import './App.css';
 import React, {useState} from 'react';
 function App() {
   const [count,setCount] = useState(0);
-  const decrement = () => {
-    setCount(count-1);
+  const decrementCount = () => {
+    setCount(count => count-1);
+    setCount(count => count-1);
   }
-  const increment = () => {
-    setCount(count+1);
+  const incrementCount = () => {
+    setCount(count => count+1);
+    setCount(count => count+1);
+    setCount(count => count+1);
   }
   return (
     <div className="App">
-      <button onClick={decrement}>-</button>
+      <button onClick={decrementCount}>-</button>
       <span>{count}</span>
-      <button onClick={increment}>+</button>
+      <button onClick={incrementCount}>+</button>
     </div>
   );
 }
